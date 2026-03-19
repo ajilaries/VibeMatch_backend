794398130353d39b947cddd01c83f8fe8e7f0245
Aju <ajilaries20@gmail.com>
2026-03-19 15:05:56 +0530

    user model for the authentication

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,12 +1,10 @@
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
-
-Base=declarative_base()
+from app.database import Base
 
 class User(Base):
-    __tablename__="users"
+    __tablename__ = "users"
 
-    id=Column(Integer,primary_key=True,index=True)
-    name=Column(String)
-    email=Column(String,unique=True)
-    password=Column(String)
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)

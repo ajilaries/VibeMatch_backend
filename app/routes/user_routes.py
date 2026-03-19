ba5be6309fdd2960b830833c986bfd52acaf4263
Aju <ajilaries20@gmail.com>
2026-03-19 10:33:54 +0530

    routes file enables authentication

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,14 +1,29 @@
 from fastapi import APIRouter
+from fastapi import HTTPException
+from app.schema.login_schema import LoginRequest
 from sqlalchemy.orm import Session
 from app.schema.user_schema import UserCreate
 from app.models.user_model import User
-from app.database import sessionLocal
-
+from app.database import SessionLocal
 router=APIRouter()
 
+
+@router.post("/login")
+def login(user):
+    db = SessionLocal()
+
+    db_user = db.query(User).filter(User.email == user.email).first()
+
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if db_user.password != user.password:
+        raise HTTPException(status_code=401, detail="Invalid password")
+
+    return {"message": "Login successful"}
 @router.post("/signup")
 def signup(user:UserCreate):
-    db:Session=sessionLocal()
+    db:Session=SessionLocal()
 
     new_user=User(
         name=user.name,

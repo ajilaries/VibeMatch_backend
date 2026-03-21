55e6344de6694cc25084e20c8d41750d2b7c1d35
Aju <ajilaries20@gmail.com>
2026-03-21 09:25:16 +0530

    database file for connectivity

--- app/database.py
+++ app/database.py
@@ -5,10 +5,23 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 
-DATABSE_URL=f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 
-engine=create_engine(DATABSE_URL)
+engine = create_engine(DATABASE_URL)
 
-SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
-Base=declarative_base()
+Base = declarative_base()
+
+
+# FastAPI dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()

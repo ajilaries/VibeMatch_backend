93e3b814ab60821e82d4c3973b6988e4a1f33768
Aju <ajilaries20@gmail.com>
2026-03-21 09:24:45 +0530

    backend running perfectly

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from app.database import engine, Base
 import app.models.user_model
 from app.routes import user_routes
+from app.routes import like_routes
+from app.routes import connection_routes
+from app.routes import match_routes
 
 
 app = FastAPI()
@@ -9,3 +12,6 @@ app = FastAPI()
 Base.metadata.create_all(bind=engine)
 
 app.include_router(user_routes.router)
+app.include_router(like_routes.router)
+app.include_router(connection_routes.router)
+app.include_router(match_routes.router)

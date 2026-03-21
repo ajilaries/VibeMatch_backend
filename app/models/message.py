e249ee74f78df6fd863a623d9c4cb727c7f55818
Aju <ajilaries20@gmail.com>
2026-03-21 09:27:19 +0530

    messages models

---
+++ app/models/message.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from app.database import Base
+
+class Message(Base):
+    __tablename__ = "messages"
+
+    id = Column(Integer, primary_key=True, index=True)
+    sender_id = Column(Integer, ForeignKey("users.id"))
+    receiver_id = Column(Integer, ForeignKey("users.id"))
+    message = Column(String)

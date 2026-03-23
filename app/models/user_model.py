c69378ef6fd2ee9c187551d8528f4c3cf7da9ce0
Aju <ajilaries20@gmail.com>
2026-03-23 13:10:23 +0530

    user model which gives the db design

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,12 +1,23 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float, Date
 from app.database import Base
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
+
+    username = Column(String, unique=True, index=True)
     email = Column(String, unique=True)
     password = Column(String)
 
+    dob = Column(Date)
 
+    bio = Column(String, nullable=True)
+
+    profile_image = Column(String, nullable=True)
+    profile_video = Column(String, nullable=True)
+
+    location = Column(String, nullable=True)
+
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)

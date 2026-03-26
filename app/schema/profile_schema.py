218ffa872227fbae86d073eed0dfd73acc15a862
Aju <ajilaries20@gmail.com>
2026-03-26 19:34:37 +0530

    profile schema for profile updation

---
+++ app/schema/profile_schema.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import date
+
+class UpdateProfileSchema(BaseModel):
+    username:Optional[str]=None
+    bio:Optional[str]=None
+    dob:Optional[date]=None
+    profile_image:Optional[str]=None
+    profile_video=Optional[str]=None
+    location=Optional[str]=None
+    

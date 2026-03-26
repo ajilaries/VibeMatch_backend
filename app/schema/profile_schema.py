77594af5c6a55365917e510629edb8eedad2303c
Aju <ajilaries20@gmail.com>
2026-03-26 20:02:09 +0530

    profile schema

--- app/schema/profile_schema.py
+++ app/schema/profile_schema.py
@@ -7,6 +7,5 @@ class UpdateProfileSchema(BaseModel):
     bio:Optional[str]=None
     dob:Optional[date]=None
     profile_image:Optional[str]=None
-    profile_video=Optional[str]=None
-    location=Optional[str]=None
-    
+    profile_video:Optional[str]=None
+    location:Optional[str]=None
